4c03c9ee9789b49e52eba4020bd03299c3a3813f
Samyak Aditya <91079592+samyak-aditya@users.noreply.github.com>
2025-02-16 13:47:37 +0530

    project setup and routes

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { PORT } from './config/env.js'
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscription Tracker APIs');
+});
+
+app.listen(PORT, () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+})
+
+export default app;
